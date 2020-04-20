7f251885b09c9206f009534d8bdadf7571b0a7e0
19Al85 <shapofall@gmail.com>
2020-04-21 00:36:55 +0300

    Добавленно отображение progressBar-ов со смещением

--- SortAlgorithms/SortedItem.cs
+++ SortAlgorithms/SortedItem.cs
@@ -7,41 +7,55 @@ using System.Windows.Forms;
 
 namespace SortAlgorithms
 {
-    class SortedItem
+    class SortedItem:IComparable
     {
         public VerticalProgressBar.VerticalProgressBar ProgressBar { get; private set; }
         public Label Label { get; private set; }
         public int Value { get; set; }
 
-        public SortedItem(int value)
+        public SortedItem(int value, int number)
         {
             Value = value;
             ProgressBar = new VerticalProgressBar.VerticalProgressBar();
             Label = new Label();
+
+            var x = number * 20;
 
             // 
             // verticalProgressBar1
             // 
             ProgressBar.BorderStyle = VerticalProgressBar.BorderStyles.Classic;
             ProgressBar.Color = System.Drawing.Color.Blue;
-            ProgressBar.Location = new System.Drawing.Point(3, 2);
+            ProgressBar.Location = new System.Drawing.Point(x, 3);
             ProgressBar.Maximum = 100;
             ProgressBar.Minimum = 0;
-            ProgressBar.Name = "verticalProgressBar1";
-            ProgressBar.Size = new System.Drawing.Size(19, 95);
+            ProgressBar.Name = "verticalProgressBar1" + number;
+            ProgressBar.Size = new System.Drawing.Size(18, 96);
             ProgressBar.Step = 1;
             ProgressBar.Style = VerticalProgressBar.Styles.Solid;
-            ProgressBar.TabIndex = 0;
+            ProgressBar.TabIndex = number;
             ProgressBar.Value = Value;
             // 
             // label3
             // 
             Label.AutoSize = true;
-            Label.Location = new System.Drawing.Point(3, 100);
-            Label.Name = "label3";
+            Label.Location = new System.Drawing.Point(x, 102);
+            Label.Name = "label"+ number;
             Label.Size = new System.Drawing.Size(19, 13);
-            Label.TabIndex = 1;
+            Label.TabIndex = number;
             Label.Text = Value.ToString();
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is SortedItem item)
+            {
+                return Value.CompareTo(item.Value);
+            }
+            else
+            {
+                throw new ArgumentException($"obj is not {nameof(SortedItem)}", nameof(obj));
+            }
         }
     }
 }
